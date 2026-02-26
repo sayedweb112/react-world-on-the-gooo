84424cb24f5c2e671fd418152e0284398b561870
unknown <112sayedalsahab@gmail.com>
2026-02-27 00:22:29 +0600

    add area,capital,population info

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 const Country = ({country}) => {
-    console.log(country.flags.flags.png)
+    console.log(country.area.area)
     return (
         <div>
-            <img src={country.flags.flags.png} alt="" />
+            <img src={country.flags.flags.png} alt={country.flags.flags.png} />
             <h3>Name: {country.name.common}</h3>
+            <h3>Capital: {country.capital.capital}</h3>
+            <h4>Area:{country.area.area}</h4>
+            <h4>Population:{country.population.population}</h4>
         </div>
     );
 };
